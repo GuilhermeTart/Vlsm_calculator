3e0c844f88f80d9de602d2f46b615343c512bd1b
GuilhermeTart <corp.tart@gmail.com>
2024-05-08 21:02:47 -0300

    new

--- VLSM_Calculate.py
+++ VLSM_Calculate.py
@@ -1,11 +1,13 @@
 import ipaddress
+import os
 from colors import colors
 
-def show_name_of_program():
-      print(f"""{colors.red}  
-            
-          
-        ____         _ _                                   _                         _        
+while True:
+        
+    def show_name_of_program():
+        print(f"""{colors.red}  
+                
+         ____         _ _                                  _                         _        
        / ___|_   _(_) | |__   ___ _ __ _ __ ___   ___     / \  _   _  __ _ _   _ ___| |_ ___  
       | |  _| | | | | | '_ \ / _ \ '__| '_ ` _ \ / _ \   / _ \| | | |/ _` | | | / __| __/ _ \ 
       | |_| | |_| | | | | | |  __/ |  | | | | | |  __/  / ___ \ |_| | (_| | |_| \__ \ || (_) |
@@ -15,42 +17,49 @@ def show_name_of_program():
             \ \   / / |___ _ __ ___    / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ 
              \ \ / /| / __| '_ ` _ \  | |   / _` | |/ __| | | | |/ _` | __/ _ \| '__|
               \ V / | \__ \ | | | | | | |__| (_| | | (__| |_| | | (_| | || (_) | |   
-               \_/  |_|___/_| |_| |_|  \____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
+               \_/  |_|___/_| |_| |_|  \____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|           
+
+                
+                
             
+                
+                {colors.reset}""")
+
+
+
+
+    def calcular_vlsm(ip_rede, num_hosts):
+        try:
+            rede = ipaddress.ip_network(ip_rede, strict=False) #Essa linha pega todos os bits de host e transforma em rede.
+            subnets = list(rede.subnets(new_prefix=32 - num_hosts.bit_length())) #Gera sub-redes baseado o no input da rede, gerando o prefixo das sub-redes,e criando os hosts em uma lista.
+            for subnet in subnets:
+                print(f"{colors.orange}Rede: {subnet.network_address}{colors.reset}")
+                print(f"{colors.orange}Broadcast: {subnet.broadcast_address}{colors.reset}")
+                print(f"{colors.orange}Máscara: {subnet.netmask} /{subnet.prefixlen}{colors.reset}")
+                print(f"{colors.green}IPs válidos:{colors.reset}")
+                for ip in list(subnet.hosts()):
+                    print(f"{colors.green}{str(ip).ljust(15)}   Máscara: {subnet.netmask} /{subnet.prefixlen}{colors.reset}")
+                print()
+                print(f"{colors.green}{'-'*74}{colors.reset}")
+                print()
+        except ValueError:
+            print(f"{colors.red}Endereço IP inválido. Por favor, insira um endereço IP válido.{colors.reset}")
+
+
+
+
+    show_name_of_program()
             
-          
-            
-            {colors.reset}""")
+    ip_rede = input(f'{colors.orange}Digite o endereço IP da rede (ex: 192.168.0.0/24): {colors.reset}')
+    num_hosts = int(input(f'{colors.orange}Digite a quantidade de hosts que deseja suportar: {colors.reset}'))
 
-
-
-
-def calcular_vlsm(ip_rede, num_hosts):
-    try:
-        rede = ipaddress.ip_network(ip_rede, strict=False) #Essa linha pega todos os bits de host e transforma em rede.
-        subnets = list(rede.subnets(new_prefix=32 - num_hosts.bit_length())) #Gera sub-redes baseado o no input da rede, gerando o prefixo das sub-redes,e criando os hosts em uma lista.
-        for subnet in subnets:
-            print(f"{colors.orange}Rede: {subnet.network_address}{colors.reset}")
-            print(f"{colors.orange}Broadcast: {subnet.broadcast_address}{colors.reset}")
-            print(f"{colors.orange}Máscara: {subnet.netmask} /{subnet.prefixlen}{colors.reset}")
-            print(f"{colors.green}IPs válidos:{colors.reset}")
-            for ip in list(subnet.hosts()):
-                print(f"{colors.green}{str(ip).ljust(15)}   Máscara: {subnet.netmask} /{subnet.prefixlen}{colors.reset}")
-            print()
-            print(f"{colors.green}{'-'*74}{colors.reset}")
-            print()
-    except ValueError:
-        print(f"{colors.red}Endereço IP inválido. Por favor, insira um endereço IP válido.{colors.reset}")
-
-
-
-
-show_name_of_program()
-        
-ip_rede = input(f'{colors.orange}Digite o endereço IP da rede (ex: 192.168.0.0/24): {colors.reset}')
-num_hosts = int(input(f'{colors.orange}Digite a quantidade de hosts que deseja suportar: {colors.reset}'))
-
-calcular_vlsm(ip_rede, num_hosts)
+    calcular_vlsm(ip_rede, num_hosts)
+    
+    Exit = input('Deseja Sair? (y/n)').lower()
+    if Exit == 'y':
+        break
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
 
